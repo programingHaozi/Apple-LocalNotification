05ecdec518d25d0b8b48aaae392d78446a0b3994
chenhao <229520281@qq.com>
2014-11-11 14:35:56 +0800

    localNotification
    localNotification

--- LocalNotification/LocalNotification/AppDelegate.m
+++ LocalNotification/LocalNotification/AppDelegate.m
@@ -17,6 +17,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //取消所有本地通知。限制最多只能存在64个通知
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
+    NSLog(@"begin===%d",[[UIApplication sharedApplication] scheduledLocalNotifications].count);
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
+    
     return YES;
 }
 
@@ -26,6 +31,40 @@
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
+    
+   
+    [[NSNotificationCenter defaultCenter]postNotificationName:@"APPWILLENTERBACKGROUND" object:nil userInfo:nil];
+    
+    // 申请额外的
+    UIApplication *app=[UIApplication sharedApplication];
+    
+    //一个后台任务标识符
+    __block UIBackgroundTaskIdentifier taskId;
+    
+    //申请后台运行更多时间
+    
+    taskId=[app beginBackgroundTaskWithExpirationHandler:^(void){
+        
+        //超出系统延长的时间执行
+        //        if (_mClient && _isLogin) {
+        //            _mClient->Logout();
+        //            _mClient->Close();
+        //        }
+        NSLog(@"Background task ran out of time and was termined.");
+        
+        [app endBackgroundTask:taskId];
+        
+    }];
+    
+    //申请失败
+    
+    if(taskId==UIBackgroundTaskInvalid){
+        NSLog(@"Fail to start background task!");
+        
+        return;
+        
+    }
+
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
@@ -40,6 +79,15 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+//收到通知
+-(void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"标题" message:notification.alertBody delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil];
+    
+    [alert show];
+    
 }
 
 @end
--- LocalNotification/LocalNotification/ViewController.m
+++ LocalNotification/LocalNotification/ViewController.m
@@ -17,6 +17,80 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(enterBackground) name:@"APPWILLENTERBACKGROUND" object:nil];
+}
+
+- (void)enterBackground{
+    NSLog(@"enter background");
+    [self performSelector:@selector(pushNotification) withObject:nil afterDelay:2.0];
+}
+
+-(void)pushNotification{
+  
+    
+    UILocalNotification *localNotification = [[UILocalNotification alloc] init];
+    
+    
+    // 设置notification的属性
+    localNotification.fireDate = [[NSDate date] dateByAddingTimeInterval:2]; //推送时间，2s后
+    
+    //时区
+    localNotification.timeZone = [NSTimeZone defaultTimeZone];
+    // 消息内容
+    localNotification.alertBody = [NSString stringWithFormat:@" 邀请你聊天"];
+    // 重复的时间间隔
+    localNotification.repeatInterval = NSSecondCalendarUnit;
+    // 触发消息时播放的声音
+    localNotification.soundName = @"ring.caf";
+  
+    //            localNotification.applicationIconBadgeNumber ++; //应用程序Badge数目
+    //            localNotification.applicationIconBadgeNumber = [[[UIApplication sharedApplication] scheduledLocalNotifications] count]+1;
+   
+    NSLog(@"push====%d",[[[UIApplication sharedApplication] scheduledLocalNotifications] count]);
+   
+    localNotification.applicationIconBadgeNumber = [[UIApplication sharedApplication] applicationIconBadgeNumber]+1;
+    
+    
+    //存入的字典，用于传入数据，区分多个通知
+    
+    NSMutableDictionary *dicUserInfo = [[NSMutableDictionary alloc] init];
+    
+    [dicUserInfo setValue:@"" forKey:@"clockID"];
+    
+    float floatHeng = 100.0;
+    
+    float floatShu = 50.0;
+    
+    [dicUserInfo setValue:[NSString stringWithFormat:@"%f",floatHeng] forKey:@"heng"];
+    
+    [dicUserInfo setValue:[NSString stringWithFormat:@"%f",floatShu] forKey:@"shu"];
+    
+    localNotification.userInfo = [NSDictionary dictionaryWithObject:dicUserInfo forKey:@"dictionary"];
+    
+    //注册
+    [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+    
+    /*
+     根据字典删除个别通知
+     
+     key:发送通知时候传入的字典值来判断是哪个推送
+     
+     for (int i=0; i<[myArray count]; i++) {
+     
+     UILocalNotification    *myUILocalNotification=[myArray objectAtIndex:i];
+     
+     if ([[[myUILocalNotification userInfo] objectForKey:@"key"] intValue]==@"字典值") {
+     
+     [[UIApplication sharedApplication] cancelLocalNotification:myUILocalNotification];
+     }
+     }
+     */
+    
+    
+
+}
+- (void)dealloc{
+    [[NSNotificationCenter defaultCenter]removeObserver:self name:@"APPWILLENTERBACKGROUND" object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
